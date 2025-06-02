3ddbf2dd2f473afc3388a295d7659a18655d71d7
Mithuna Malini <mithunamalini1409@gmail.com>
2025-06-02 12:27:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProfileCard from './Component/ProfileCard';
-import About from './Component/About';
 import Education from './Component/Education';
 
 function App() {
   return (
-    <Router>
-      <div className="App" style={{ textAlign: 'center', background: '#0a0a0a', color: 'white', minHeight: '100vh', padding: '20px' }}>
+    <Router basename="/my-portfolio">
+      <div
+        className="App"
+        style={{
+          textAlign: 'center',
+          background: '#0a0a0a',
+          color: 'white',
+          minHeight: '100vh',
+          padding: '20px'
+        }}
+      >
         <Routes>
           <Route path="/" element={<ProfileCard />} />
-          <Route path="/education" element={<Education />} />
-          {/* Add more routes as needed */}
+          <Route path="/education" element={<Education />} />  {/* Remove /my-portfolio here */}
         </Routes>
       </div>
     </Router>
